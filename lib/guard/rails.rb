39052564a9afcb0510a09aa91ede0fb9c01e6726
John Bintz <john@coswellproductions.com>
2011-05-27 14:58:20 -0400

    stop rails when shutting down

--- lib/guard/rails.rb
+++ lib/guard/rails.rb
@@ -17,12 +17,25 @@ module Guard
 
     def run_all
       Notifier.notify("Rails restarting on port #{options[:port]} in #{options[:environment]}", :title => "Restarting Rails...", :image => :pending)
-      system %{sh -c '[[ -f tmp/pids/#{options[:environment]}.pid ]] && kill $(cat tmp/pids/#{options[:environment]}.pid)'}
-      system %{rails s -d -e #{options[:environment]} -p #{options[:port]}}
+      stop_rails ; start_rails
+    end
+
+    def stop
+      Notifier.notify("Until next time...", :title => "Rails shutting down.", :image => :pending)
+      stop_rails
     end
 
     def run_on_change(paths)
       run_all
+    end
+
+    private
+    def start_rails
+      system %{rails s -d -e #{options[:environment]} -p #{options[:port]}}
+    end
+
+    def stop_rails
+      system %{sh -c '[[ -f tmp/pids/#{options[:environment]}.pid ]] && kill $(cat tmp/pids/#{options[:environment]}.pid)'}
     end
   end
 end
